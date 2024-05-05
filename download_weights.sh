e991bd3f1acbfdc1748f75517e29ff0307f18c5d
Aishwarya Chakravarthy <67703406+AishwaryaC26@users.noreply.github.com>
2024-05-05 15:38:47 -0400

    Update download_weights.sh

--- download_weights.sh
+++ download_weights.sh
@@ -2,12 +2,15 @@
 
 WEIGHTS_PATH="./unitable_weights"
 
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/vqvae_1m.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/vqvae_2m.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/ssp_1m_base.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/ssp_1m_large.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/ssp_2m_base.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/ssp_2m_large.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/unitable_large_structure.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/unitable_large_bbox.pt -P "$WEIGHTS_PATH"
-wget -c https://huggingface.co/poloclub/UniTable/resolve/main/unitable_large_content.pt -P "$WEIGHTS_PATH"
+mkdir -p "$WEIGHTS_PATH"
+
+curl -L -o "$WEIGHTS_PATH/vqvae_1m.pt" https://huggingface.co/poloclub/UniTable/resolve/main/vqvae_1m.pt
+curl -L -o "$WEIGHTS_PATH/vqvae_2m.pt" https://huggingface.co/poloclub/UniTable/resolve/main/vqvae_2m.pt
+curl -L -o "$WEIGHTS_PATH/ssp_1m_base.pt" https://huggingface.co/poloclub/UniTable/resolve/main/ssp_1m_base.pt
+curl -L -o "$WEIGHTS_PATH/ssp_1m_large.pt" https://huggingface.co/poloclub/UniTable/resolve/main/ssp_1m_large.pt
+curl -L -o "$WEIGHTS_PATH/ssp_2m_base.pt" https://huggingface.co/poloclub/UniTable/resolve/main/ssp_2m_base.pt
+curl -L -o "$WEIGHTS_PATH/ssp_2m_large.pt" https://huggingface.co/poloclub/UniTable/resolve/main/ssp_2m_large.pt
+curl -L -o "$WEIGHTS_PATH/unitable_large_structure.pt" https://huggingface.co/poloclub/UniTable/resolve/main/unitable_large_structure.pt
+curl -L -o "$WEIGHTS_PATH/unitable_large_bbox.pt" https://huggingface.co/poloclub/UniTable/resolve/main/unitable_large_bbox.pt
+curl -L -o "$WEIGHTS_PATH/unitable_large_content.pt" https://huggingface.co/poloclub/UniTable/resolve/main/unitable_large_content.pt
+
