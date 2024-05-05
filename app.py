e2a51f02beb0b95c90dbcce317fb455ac7673974
Aishwarya Chakravarthy <67703406+AishwaryaC26@users.noreply.github.com>
2024-05-05 11:40:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -254,6 +254,6 @@ async def predict(image: UploadFile = File(...)):
     return {'prediction': table_code}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run("app:app", host='127.0.0.1', port=8000, workers=3)
 
 
